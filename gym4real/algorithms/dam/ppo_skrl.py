37834d0d25d1e0d557bc0642d32ebbdd4fb953c0
samuele <samuele.delpero@mail.polimi.it>
2025-05-16 12:37:02 +0200

    debug paths

--- gym4real/algorithms/dam/ppo_skrl.py
+++ gym4real/algorithms/dam/ppo_skrl.py
@@ -1,3 +1,8 @@
+import os
+import sys
+
+sys.path.append(os.getcwd())
+
 import gymnasium as gym
 import numpy as np
 
@@ -125,7 +130,7 @@ def train_ppo(params, args, eval_env_params, device='cuda'):
     # logging to TensorBoard and write checkpoints (in timesteps)
     cfg["experiment"]["write_interval"] = 500
     cfg["experiment"]["checkpoint_interval"] = 500
-    cfg["experiment"]["directory"] = args.get('save_dir', "../../algorithms/dam/runs")
+    cfg["experiment"]["directory"] = args.get('save_dir', "gym4real/algorithms/dam/runs")
 
     agent = PPO(models=models,
                 memory=memory,
@@ -187,11 +192,11 @@ if __name__ == '__main__':
     }
 
     params = parameter_generator(
-        world_options='../../envs/dam/world_train.yaml',
-        lake_params='../../envs/dam/lake.yaml')
+        world_options='gym4real/envs/dam/world_train.yaml',
+        lake_params='gym4real/envs/dam/lake.yaml')
 
     eval_params = parameter_generator(
-        world_options='../../envs/dam/world_test.yaml',
-        lake_params='../../envs/dam/lake.yaml')
+        world_options='gym4real/envs/dam/world_test.yaml',
+        lake_params='gym4real/envs/dam/lake.yaml')
 
     train_ppo(params, args, eval_params)
